0817b9f3c741b014c48cb750d481b88c3c8d9bbf
ragritti <116258047+ragritti@users.noreply.github.com>
2025-06-17 20:05:12 +0530

    added creds

---
+++ src/app/api/survey-data-collection/route.js
@@ -0,0 +1,17 @@
+import connectMongoDB from "@/app/libs/mongodb"
+import SurveyData from "@/app/models/SurveyData";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+    const {  Name, Email, data,status,error } = await req.json();
+    await connectMongoDB();
+    const newSurvey = new SurveyData({
+        email: Email,
+        OrganizationName: Name,
+        data,
+        status,
+        error
+    })
+    const res=await newSurvey.save()
+    return NextResponse.json({message:"Survey data saved"})
+}
